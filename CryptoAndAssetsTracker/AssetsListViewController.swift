7eb4c28862b91b00c551ef8b0702869455d5e477
Kevin Taniguchi <ktaniguchi@urbn.com>
2018-02-27 16:45:03 -0600

    Remove storyboards set up main vcs set up main tests

---
+++ CryptoAndAssetsTracker/AssetsListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AssetsListViewController.swift
+//  CryptoAndAssetsTracker
+//
+//  Created by Kevin Taniguchi on 2/27/18.
+//  Copyright © 2018 Taniguchi. All rights reserved.
+//
+
+import Foundation
